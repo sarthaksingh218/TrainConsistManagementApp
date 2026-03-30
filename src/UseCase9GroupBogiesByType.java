ba8e60a7dfc0a77988f2fa2a8fdb2f94afc78ce6
sarthaksingh218 <ss5652@srmist.edu.in>
2026-03-30 14:48:51 +0530

    UC9: Group Bogies by Type (Collectors.groupingBy)

--- src/UseCase9GroupBogiesByType.java
+++ src/UseCase9GroupBogiesByType.java
@@ -23,7 +23,7 @@ class Bogie {
     }
 }
 
-public class UseCase8FilterPassengerBogies {
+public class UseCase9GroupBogiesByType {
 
     public static void main(String[] args) {
 
@@ -33,26 +33,30 @@ public class UseCase8FilterPassengerBogies {
 
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("Sleeper", 70));
         bogies.add(new Bogie("First Class", 24));
-        bogies.add(new Bogie("Luxury Coach", 80));
+        bogies.add(new Bogie("AC Chair", 60));
 
         System.out.println("Original Bogie List:");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
 
-        List<Bogie> filteredBogies =
+        Map<String, List<Bogie>> groupedBogies =
                 bogies.stream()
-                        .filter(b -> b.getCapacity() > 60)
-                        .collect(Collectors.toList());
+                        .collect(Collectors.groupingBy(Bogie::getName));
 
-        System.out.println("\nFiltered Bogies (Capacity > 60):");
-        for (Bogie b : filteredBogies) {
-            System.out.println(b);
+        System.out.println("\nGrouped Bogies By Type:");
+
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println("\nType: " + entry.getKey());
+            for (Bogie b : entry.getValue()) {
+                System.out.println(b);
+            }
         }
 
         System.out.println("\nOriginal List Size: " + bogies.size());
-        System.out.println("Filtered List Size: " + filteredBogies.size());
+        System.out.println("Number of Groups: " + groupedBogies.size());
 
         System.out.println("Program continues...");
     }
