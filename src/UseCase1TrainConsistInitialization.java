f42e70e5bcec58d170f549a3d87d0cd636532e21
sarthaksingh218 <ss5652@srmist.edu.in>
2026-03-30 14:40:04 +0530

    UC1: Display Consist Summary

---
+++ src/UseCase1TrainConsistInitialization.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class UseCase1TrainConsistInitialization {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train consist initialized.");
+
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+
+        System.out.println("Program continues...");
+    }
+}
